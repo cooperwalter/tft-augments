92b4638c09da51f28923d2288aefc73e558779b7
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2023-03-03 15:59:17 -0800

    add source of data

--- src/modules/augments/rawData.js
+++ src/modules/augments/rawData.js
@@ -1,4 +1,5 @@
 // Raw augment probability data in single-line format
+// Data from: https://dotesports.com/tft/news/how-teamfight-tactics-set-8-augments-work
 // 2023-03-02
 const data =
   "12%	Hero	Gold	Gold 12%	Gold	Gold	Hero 10%	Gold	Hero	Gold 8%	Hero	Silver	Gold 7%	Silver	Hero	Gold 7%	Silver	Gold	Hero 6%	Gold	Hero	Silver 5%	Gold	Prismatic	Hero 5%	Prismatic	Gold	Hero 4%	Hero	Prismatic	Gold 4%	Gold	Gold	Gold 4%	Prismatic	Hero	Gold 3%	Hero	Silver	Prismatic 3%	Hero	Gold	Prismatic 3%	Silver	Silver	Hero 2%	Gold	Hero	Prismatic 1%	Silver	Hero	Prismatic 1%	Silver	Gold	Silver 1%	Silver	Gold	Prismatic 1%	Gold	Silver	Gold 1%	Prismatic	Prismatic	Prismatic";
