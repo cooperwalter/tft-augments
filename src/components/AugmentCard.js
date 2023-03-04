60a39b5f1c69e154ddc2e2a2f371f8f747398e58
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2023-03-04 11:36:14 -0800

    fix size of augment cards

--- src/components/AugmentCard.js
+++ src/components/AugmentCard.js
@@ -1,12 +1,27 @@
 /** @jsxImportSource @emotion/react */ // need to add this in every file? :(
 import _ from "lodash";
-import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+  css,
+} from "@mui/material";
 
 const AugmentCard = ({ onClick, children, type, probability, ...rest }) => {
   return (
     <Card>
       <CardActionArea onClick={onClick} {...rest}>
-        <CardContent>
+        <CardContent
+          css={css`
+            width: 12.5vh;
+            height: 12.5vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          `}
+        >
           <Typography>{_.upperFirst(type)}</Typography>
           <Typography>{probability * 100}%</Typography>
         </CardContent>
